8d6182d242ad1f403f5f014532bd707754171175
nikolairaahauge <nira36696@edu.ucl.dk>
2023-04-12 03:22:27 +0200

    Created Strapi project

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd1a0a0a02883cf8c3830abbdf69a613'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b28e8aaa-dd7d-4cdf-8c95-915fe1543b8d'
+};
